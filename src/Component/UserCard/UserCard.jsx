62c0f3c8715c2af681cfadcc3a65e424ee354a86
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-05-08 10:02:36 +0300

    change logo element

--- src/Component/UserCard/UserCard.jsx
+++ src/Component/UserCard/UserCard.jsx
@@ -1,26 +1,26 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { getUsers } from "../../service/getQuery";
-import User from "../User/User";
-import css from "./UserCard.module.css";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { getUsers } from '../../service/getQuery';
+import User from '../User/User';
+import css from './UserCard.module.css';
 
 function UserCard() {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
-    getUsers(page).then((data) => setUsers((prev) => [...prev, ...data]));
+    getUsers(page).then(data => setUsers(prev => [...prev, ...data]));
   }, [page]);
 
   const handelClick = () => {
-    setPage((prevPage) => prevPage + 1);
+    setPage(prevPage => prevPage + 1);
   };
   return (
     <>
       {users?.map(({ followers, id, avatar, tweets, following }) => (
         <div key={id} className={css.card}>
           <div className={css.bgi_wrapp}>
-            <img className={css.logo} src="../src/assets/logo.png" alt="Logo" />
+            <div className={css.logo_wrapp}></div>
             <div className={css.border_bottom}></div>
           </div>
           <div className={css.user_wrapp}>
