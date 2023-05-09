e7b378a7ac3fa605345a106ba06f327dea0252e7
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-05-09 14:15:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/new-test-react-project/",
-  plugins: [react(/text-react-project/)],
+  base: '/new-test-react-project',
+  plugins: [react(/new-test-react-project/)],
 });
