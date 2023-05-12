c59d78bc9f02ec08b36f6cbae47f2bbe518966c4
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-05-12 22:23:40 +0300

    change react dom base name

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/new-test-react-project/">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
