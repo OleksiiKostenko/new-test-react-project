60687a57ff63a43a39ae2d6f0ca9c387f30f05f7
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-05-12 22:10:09 +0300

    fix react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Component/App/App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/new-test-react-project/">
+    <HashRouter basename="/new-test-react-project/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
